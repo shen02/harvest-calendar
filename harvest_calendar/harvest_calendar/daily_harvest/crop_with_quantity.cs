53f7251f350d13ab8a8c7b4a90fda8335ee3b286
shen02 <azshen79@gmail.com>
2025-11-16 19:44:12 -0500

    rename for naming convention

--- harvest_calendar/harvest_calendar/daily_harvest/crop_with_quantity.cs
+++ harvest_calendar/harvest_calendar/daily_harvest/crop_with_quantity.cs
@@ -6,22 +6,22 @@ namespace HarvestCalendar.DailyHarvestInfo;
 // Invariant: all crops used to instatiate a CropWithQuantity object are harvestable at the time of instantiation, hence quantity > 0;
 internal sealed class CropWithQuantity
 {
-    private Crop crop;
-    private int quantity = 0;
+    private Crop _crop;
+    private int _quantity = 0;
 
     public CropWithQuantity(Crop c, int q)
     {
-        crop = c;
-        quantity = q;
+        _crop = c;
+        _quantity = q;
     }
 
     public Crop getCrop()
     {
-        return this.crop;
+        return this._crop;
     }
 
     public int getQuantity()
     {
-        return this.quantity;
+        return this._quantity;
     }
 }
